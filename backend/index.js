f510f5f02cdf1457db2e7279ded67ad609de04d5
Raghav <raghavk2806@gmail.com>
2024-11-13 02:41:21 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors())
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://attendance-pink.vercel.app/");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
