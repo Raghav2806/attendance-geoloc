54a0d56f31b29b5885e6d77c7a3dc3ed33816ce8
Raghav <raghavk2806@gmail.com>
2024-11-13 15:17:12 +0530

    gps

--- backend/index.js
+++ backend/index.js
@@ -19,10 +19,12 @@ app.use(
   })
 );
 
+// app.use(cors());
+
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://attendance-pink.vercel.app"
+    "https://attendance-pink.vercel.app" //*
   );
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
