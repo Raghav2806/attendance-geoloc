37bea1f4b907ee384ea0bd01497fb66b297685da
Raghav <raghavk2806@gmail.com>
2024-11-15 00:59:14 +0530

    await-controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,5 @@
 import { accessChecker, genQr } from "../services/userServices.js";
 import { ApiError } from "../errors/ApiError.js";
-import * as fs from "fs";
 
 export async function generateQr(req, res, next) {
   try {
@@ -15,38 +14,9 @@ export async function generateQr(req, res, next) {
   }
 }
 
-export async function getUrl(req, res, next) {
-  try {
-    const filePath = "url.json";
-
-    fs.readFile(filePath, "utf-8", (err, data) => {
-      if (err) {
-        return res.status(500).json({ message: "Failed to retrieve URL" });
-      }
-
-      if (!data) {
-        return res.status(200).json({ url: null }); // Send a response with a null URL
-      }
-
-      try {
-        const urlData = JSON.parse(data);
-        res.status(200).json(urlData);
-      } catch (parseError) {
-        res.status(500).json({ message: "Invalid JSON format in URL file" });
-      }
-    });
-  } catch (err) {
-    if ((err.message = "User already exists")) {
-      next(ApiError.badRequest(err.message));
-    } else {
-      next(ApiError.badRequest(err.message));
-    }
-  }
-}
-
 export async function checkAccess(req, res, next) {
   try {
-    const access = accessChecker(req.body, req.params);
+    const access = await accessChecker(req.body, req.params);
     res.status(201).json({ message: "Event saved.", accessGranted: access });
   } catch (err) {
     if ((err.message = "User already exists")) {
