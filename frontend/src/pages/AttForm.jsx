7105a00650fcd1a297858666e0c2a698bce16b3e
Raghav <raghavk2806@gmail.com>
2024-11-16 14:45:41 +0530

    ui

--- frontend/src/pages/AttForm.jsx
+++ frontend/src/pages/AttForm.jsx
@@ -2,44 +2,66 @@ import { useEffect, useState } from "react";
 import { useParams, Form } from "react-router-dom";
 import { getCurrentLocation } from "./HomePage";
 
+import classes from "./AttForm.module.css";
+
 export default function AttForm() {
-  const {formId} = useParams();
+  const { formId } = useParams();
   const [accessGranted, setAccessGranted] = useState(false);
+  const [checking, setChecking] = useState(false);
 
   useEffect(() => {
     async function checkLocationAccess() {
-      try{
-          const location = await getCurrentLocation();
-          
-          const response = await fetch(
-            `https://attendance-geoloc.onrender.com/verify-access/${formId}`,
-            {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify(location),
-            }
-          );
+      try {
+        const location = await getCurrentLocation();
 
-          const data = await response.json();
-          setAccessGranted(data.accessGranted);
-        } catch (err) {
-          setAccessGranted(false);
-        }
-        
+        const response = await fetch(
+          `https://attendance-geoloc.onrender.com/verify-access/${formId}`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(location),
+          }
+        );
+
+        const data = await response.json();
+        setAccessGranted(data.accessGranted);
+      } catch (err) {
+        setAccessGranted(false);
+      }
     }
+    setChecking(true);
     checkLocationAccess();
+    setChecking(false);
   }, [formId]);
 
   return (
     <div>
-      {accessGranted ? (
+      {checking ? (
+        <p>Checking your location...</p>
+      ) : accessGranted ? (
         <Form method="post">
-          <label>BITS ID</label>
-          <input type="text" name="bits_id" required />
-          <button type="submit">Submit</button>
+          <div className="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="floatingInput"
+              placeholder="BITS ID"
+              name="bits_id"
+              required
+            />
+            <label for="floatingInput">BITS ID</label>
+          </div>
+          <div className={classes.centre}>
+            <button type="submit" className="btn btn-light">
+              Submit
+            </button>
+          </div>
         </Form>
       ) : (
-        <p>Access Denied: You must be in the class to gain access.</p>
+        <p>
+          Access Denied: You must be in the class to gain access. Please provide
+          access to location if you are in the class.
+        </p>
       )}
     </div>
   );
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -28,6 +28,7 @@ export function getCurrentLocation() {
 export default function HomePage() {
   const [url, setUrl] = useState(null);
   const [location, setLocation] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getCurrentLocation()
@@ -37,6 +38,7 @@ export default function HomePage() {
 
   async function handleGenerateQr() {
     if (location) {
+      setLoading(true);
       const response = await fetch(`https://attendance-geoloc.onrender.com/gen-url`, {
         method: "POST",
         headers: {
@@ -47,13 +49,22 @@ export default function HomePage() {
 
       const data = await response.json();
       setUrl(data.url);
+      setLoading(false);
     }
   }
   return (
     <>
-      <button onClick={handleGenerateQr}>Generate QR</button>
+    <div className={classes.containerr}>
+      <button className="btn btn-light" onClick={handleGenerateQr}>Generate QR</button>
       <div className={classes.qr}>
-        {url ? <QRCodeSVG value={url} /> : <p>No url</p>}
+      {loading ? (
+          <p>Loading...</p> 
+        ) : url ? (
+          <QRCodeSVG value={url} />
+        ) : (
+          <p>Please generate a QR</p>
+        )}
+      </div>
       </div>
     </>
   );
