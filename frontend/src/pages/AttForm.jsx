d8dc717066045f3aad04b83438b952d2c58a53d6
Raghav <raghavk2806@gmail.com>
2024-11-27 12:58:36 +0530

    prettier

--- frontend/src/pages/AttForm.jsx
+++ frontend/src/pages/AttForm.jsx
@@ -45,32 +45,32 @@ export default function AttForm() {
   async function handleSuccess(response) {
     setIsLoggingIn(true);
     try {
-    const { email, hd } = jwtDecode(response.credential);
-    const requestBody = {
-      email,
-      formId,
-    };
+      const { email, hd } = jwtDecode(response.credential);
+      const requestBody = {
+        email,
+        formId,
+      };
 
-    if (hd == "pilani.bits-pilani.ac.in") {
-      const response = await fetch(
-        `https://attendance-geoloc.onrender.com/mark-attendance`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({requestBody}),
+      if (hd == "pilani.bits-pilani.ac.in") {
+        const response = await fetch(
+          `https://attendance-geoloc.onrender.com/mark-attendance`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ requestBody }),
+          }
+        );
+
+        if (response.ok) {
+          window.location.href = "/success"; // Redirect on success
+        } else {
+          console.error("Error marking attendance.");
         }
-      );
-
-      if (response.ok) {
-        window.location.href = "/success"; // Redirect on success
       } else {
-        console.error("Error marking attendance.");
+        setMailChecker(true);
       }
-    } else {
-      setMailChecker(true);
-    }
     } catch (err) {
       console.log(err);
     } finally {
@@ -93,10 +93,18 @@ export default function AttForm() {
           <p>Please login using your BITS email to mark your attendance.</p>
           {isLoggingIn ? (
             <p className={classes.message}>Logging in, please wait...</p>
-          ) :(<div className={classes.googleLogin}><GoogleOAuthProvider clientId={googleClientId}>
-            <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
-          </GoogleOAuthProvider></div>)}
-          {mailChecker && (<p className={classes.error}>Please use your BITS Mail ID to mark attendance</p>)}
+          ) : (
+            <div className={classes.googleLogin}>
+              <GoogleOAuthProvider clientId={googleClientId}>
+                <GoogleLogin onSuccess={handleSuccess} onError={handleError} />
+              </GoogleOAuthProvider>
+            </div>
+          )}
+          {mailChecker && (
+            <p className={classes.error}>
+              Please use your BITS Mail ID to mark attendance
+            </p>
+          )}
         </div>
       ) : (
         <p className={classes.message}>
